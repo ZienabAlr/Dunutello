322d12ca06a80112bff388e7abf959ffc080b112
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-11 21:37:05 +0100

    feat(dunutello.js): get request added

--- api/routers/api/v1/dunutello.js
+++ api/routers/api/v1/dunutello.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// GET /api/v1/dunutello  
+
+router.get('/', (req, res) => {
+    res.json({
+    
+        "status": "success",
+        "data": {
+            
+        }
+
+        
+    });
+});
+
+
+
 
