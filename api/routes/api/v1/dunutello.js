6c247a9271928ac05657a7ca790be4127a443a84
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-11 21:41:55 +0100

    fix(api): api, v1 folders and dunutello.js file have been moved to routes folder

---
+++ api/routes/api/v1/dunutello.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /api/v1/dunutello  
+
+router.get('/', (req, res) => {
+    res.json({
+    
+        "status": "success",
+        "data": {
+
+        }
+
+        
+    });
+});
+
+// GET /api/v1/dunutello/:id
+
+router.get('/:id', (req, res) => {
+    res.json({
+    
+        "status": "success",
+        "data": {
+
+        }
+
+        
+    });
+});
+
+
+
+
+
+
+
+
