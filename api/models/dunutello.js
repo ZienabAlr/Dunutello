c50227ecc20029fb346c41bfd31c2b0495766bad
ZienabAlr <r0784494@student.thomasmore.be>
2022-12-12 21:47:47 +0100

    feat(dunutello.js): schema in folder models is made

--- api/models/dunutello.js
+++ api/models/dunutello.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const dunutelloSchema = new Schema({
+    company:{type: String, required: true},
+    email: {type: String, required: true},
+    phone:{type: String, required: true},
+    dountname: {type: String, required: true}, 
+    glaze: {type: String, required: true},
+    toppings: {type: String, required: true},
+    logoimg: {type: String, required: true},
+    logoshape: {type: String, required: true},
+    amount: {type: Number, required: true},
+    note: {type: String, required: true}
+});
+
+
+
